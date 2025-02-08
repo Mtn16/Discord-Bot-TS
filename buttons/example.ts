26bb3a67d4fcabf82374a31c870d58adf004ee90
Martin Góč <104649436+Mtn16@users.noreply.github.com>
2025-02-08 12:33:46 +0100

    Delete buttons directory

--- buttons/example.ts
+++
@@ -1,8 +0,0 @@
-import { ButtonBuilder, ButtonInteraction } from "discord.js";
-import Button from "../types/Button";
-
-export default class ExampleButton implements Button {
-     customId = "example_button"
-     data = new ButtonBuilder()
-     async execute (interaction: ButtonInteraction) {}
-}
