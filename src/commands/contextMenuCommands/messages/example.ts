810a9078964a82c4ec965018e4d032e7edf54063
Martin Góč <104649436+Mtn16@users.noreply.github.com>
2025-02-08 12:35:23 +0100

    Delete src/commands/contextMenuCommands/messages directory

--- src/commands/contextMenuCommands/messages/example.ts
+++
@@ -1,7 +0,0 @@
-import { ApplicationCommandType, ContextMenuCommandBuilder, MessageContextMenuCommandInteraction } from "discord.js";
-import { MessageContextMenuCommand } from "../../../types/ContextMenuCommand";
-
-export default class ExampleMessageContextCommand implements MessageContextMenuCommand {
-     data = new ContextMenuCommandBuilder().setName("Example").setType(ApplicationCommandType.Message)
-     async execute (interaction: MessageContextMenuCommandInteraction) {}
-}
